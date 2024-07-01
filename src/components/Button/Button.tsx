42c02917c4960b152995ed35d1168ce3be289717
¨David <davidl1199@hotmail.com>
2024-07-01 14:45:06 -0300

    refactor: update generic button to receive bgColor, hoverColor and padding through props values.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,13 +3,16 @@ import React, { ComponentProps } from "react";
 type ButtonProps = ComponentProps<"button"> & {
   text?: string
   icon?: React.ReactNode
+	bgColor?: string
+	hoverColor?: string
+	padding?: string
 }
 
-function Button({ text, icon, ...restProps }: ButtonProps) {
+function Button({ text, icon, bgColor, hoverColor, padding, ...restProps }: ButtonProps) {
 	return (
 		<button {...restProps}>
 			{text && text}
-			{icon && <span className="bg-coffeeVariant1 hover:bg-coffeeVariant2 p-2 rounded-full">{icon}</span>}
+			{icon && <span className={`${bgColor} hover:${hoverColor} ${padding}  rounded-full`}>{icon}</span>}
 		</button>
 	);
 }
